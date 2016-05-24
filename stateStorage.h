3db3a806d423a3a7385d52fbeab9371f82b5e514
dustan1 <goadustan@gmail.com>
2016-05-24 09:51:47 -0400

    Things

--- stateStorage.h
+++ stateStorage.h
@@ -2,6 +2,12 @@
 #ifndef STATESTORAGE_H_INCLUDED
 #define STATESTORAGE_H_INCLUDED
 
+#include <cstdio>
+#include <cstring>
+
+#define NUM_KEYS 256
+
+
 //Structs
 //{
 
@@ -12,16 +18,13 @@ typedef struct mouseInfo
    int clickType;
 };
 
-typedef struct keyInfo
-{
-   bool keys[256];
-};
-
 //}
 
 class stateStorage
 {
 public:
+
+   int numKeys;
 
    //Constructors and Destructors
    inline stateStorage();
@@ -35,9 +38,9 @@ public:
    inline void resetCurrentKey();
 
    //Getting functions
-   inline mouseInfo getMouse();
-   inline keyInfo getKeyboard();
-   inline keyInfo getCurrentKey();
+   inline mouseInfo* getMouse();
+   inline bool* getKeyboard();
+   inline bool* getCurrentKey();
 
    //Setting functions
    //They have to be public because of outside callers.
@@ -49,15 +52,99 @@ public:
 private:
 
 
-
-
-
-
+   mouseInfo mouseState;
+   bool keyState[NUM_KEYS];
+   bool currentKeyState[NUM_KEYS];
 
 };
 
 
+//Implementation
+//{
 
+   //Constructor and Destructor
+   //{
+
+   stateStorage::stateStorage()
+   {
+      numKeys = NUM_KEYS;
+      resetAll();
+   }
+
+   stateStorage::~stateStorage()
+   {
+      resetAll();
+   }
+
+   //}
+
+
+   //Reseting functions
+   //{
+
+   void stateStorage::resetAll()
+   {
+      resetMouse();
+      resetKeyboard();
+      resetCurrentKey();
+   }
+
+   void stateStorage::resetMouse()
+   {
+      mouseState.x = 0; mouseState.y = 0; mouseState.clickType = 0;
+   }
+
+   void stateStorage::resetKeyboard()
+   {
+      memset(keyState, 0, numKeys);
+   }
+
+   void stateStorage::resetCurrentKey()
+   {
+      memset(currentKeyState, 0, numKeys);
+   }
+
+   //}
+
+
+   //Getting functions
+   //{
+
+   mouseInfo* stateStorage::getMouse()
+   {
+      return &mouseState;
+   }
+
+   bool* stateStorage::getKeyboard()
+   {
+      return keyState;
+   }
+
+   bool* stateStorage::getCurrentKey()
+   {
+      return currentKeyState;
+   }
+
+   //}
+
+   //Setting functions
+   //{
+
+   void stateStorage::setMouse(int xPos, int yPos, int mouseButton)
+   {
+      mouseState.x = xPos; mouseState.y = yPos; mouseState.clickType = mouseButton;
+   }
+
+   void stateStorage::setKeyboard(char keyDown)
+
+
+   void stateStorage::setCurrentKey(char keyDown)
+
+
+
+   //}
+
+//}
 
 
 extern stateStorage* stateInfo;
