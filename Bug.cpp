b43e176fe9c082c8684e79326bf73a117458cf9e
dustan1 <goadustan@gmail.com>
2017-06-08 22:38:27 -0400

    adfwe

--- Bug.cpp
+++ Bug.cpp
@@ -6,6 +6,32 @@ Bug::Bug()
    return;
 }
 
+Bug::Bug(int aproxSeverity)
+{
+   int weights[BUG_TYPE_NUMBER] = {0}; //All bug chances start at 0.
+   if (aproxSeverity == 0){
+      weights[BUG_BAD_DESIGN] = 50
+   }
+   else if (aproxSeverity < 2){
+
+   }
+   else if (aproxSeverity < 4){
+
+   }
+   else if (aproxSeverity < 6){
+      //
+   }
+   else if (aproxSeverity < 8){
+      //
+   }
+   else {
+      //
+   }
+   trueSeverity = aproxSeverity; //TODO
+}
+
+Bug::Bug(bugTypes type)
+
 Bug::~Bug()
 {
    //dtor
--- Bug.h
+++ Bug.h
@@ -2,7 +2,10 @@
 #define BUG_H
 
 #include <string>
+#include <stdlib.h>
 
+
+#define BUG_TYPE_NUMBER 8
 enum bugTypes {BUG_OVERFLOW, BUG_UNDERFLOW, BUG_OVERREAD, BUG_UNSANITIZED_INPUT, BUG_RACE_CONDITION,
                   BUG_LOGIC_ERROR, BUG_BAD_DESIGN, BUG_BACKDOOR}; //Maybe more flavour?
 enum Severity {SEV_SOLVED, SEV_BENIGN, SEV_LOW, SEV_MEDIUM, SEV_HIGH, SEV_CRITICAL};
@@ -12,6 +15,10 @@ class Bug
    public:
       /** Default constructor */
       Bug();
+
+      Bug(int aproxSeverity);
+      Bug(bugTypes type);
+
       /** Default destructor */
       virtual ~Bug();
 
